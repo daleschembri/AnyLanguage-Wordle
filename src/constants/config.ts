8202a48bb1678ba122c1693a226fd8d42a7fdf72
daleschembri <44404912+daleschembri@users.noreply.github.com>
2022-02-04 16:19:34 +0100

    Wordlist Changes

--- src/constants/config.ts
+++ src/constants/config.ts
@@ -11,5 +11,5 @@ export const CONFIG = {
   //
   googleAnalytics: '', // You can use this if you use Google Analytics
   shuffle: false, // whether to shuffle the words in the wordlist each time you load the app (note: you will lose the 'word of the day' functionality if this is true)
-  normalization: 'NFC', // whether to apply Unicode normalization to words and orthography - options: 'NFC', 'NFD', 'NKFC', 'NKFD', false
+  normalization: false, // whether to apply Unicode normalization to words and orthography - options: 'NFC', 'NFD', 'NKFC', 'NKFD', false
 }
